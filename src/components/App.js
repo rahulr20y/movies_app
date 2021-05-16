f76eb1405d1bb30a073bf2323f955aef11f8194b
Rahul Yadav <rahulr20y@gmail.com>
2021-05-16 10:43:55 +0530

    Add Cobime Reducers method

--- src/components/App.js
+++ src/components/App.js
@@ -20,10 +20,10 @@ class App extends React.Component {
   }
 
   isMovieFavourite = (movie) => {
-     const { favourites } = this.props.store.getState();
+     const { movies } = this.props.store.getState();
     //let favourites = this.props.store.getState().favourites;
 
-    const index = favourites.indexOf(movie);
+    const index = movies.favourites.indexOf(movie);
     //console.log(index)
     if (index !== -1){
       return true;
@@ -36,34 +36,35 @@ class App extends React.Component {
   }
 
   render() {
-  const {list, favourites, showFavourites} = this.props.store.getState(); // {list: [] , favourites: []}
-  console.log('RENDER',this.props.store.getState());
-  
-  const displayMovies = showFavourites ? favourites : list;
-  return (
-      <div className="App">
-        <Navbar />
-        <div className="main">
-          <div className="tabs">
-            <div className={`tab ${showFavourites ? 'active-tabs' : ''}`} onClick={() => this.onChangeTab(false)}>Movies</div>
-            <div className={`tab ${showFavourites ? 'active-tabs' : ''}`} onClick={() => this.onChangeTab(true)}>Favourites</div>
-          </div>
+    const {movies} = this.props.store.getState(); // {movies: {} , search: {}}
+    const {list, favourites, showFavourites} = movies;
+    console.log('RENDER',this.props.store.getState());
+    
+    const displayMovies = showFavourites ? favourites : list;
+    return (
+        <div className="App">
+          <Navbar />
+          <div className="main">
+            <div className="tabs">
+              <div className={`tab ${showFavourites ? '' : 'active-tabs'}`} onClick={() => this.onChangeTab(false)}>Movies</div>
+              <div className={`tab ${showFavourites ? 'active-tabs' : ''}`} onClick={() => this.onChangeTab(true)}>Favourites</div>
+            </div>
 
-          <div className="list">
-              {displayMovies.map( (movie, index) => (
-                  <MovieCard 
-                    movie={movie}
-                    key={`movies-${index}`}
-                    dispatch={this.props.store.dispatch}
-                    isFavorite = {this.isMovieFavourite(movie)}
-                  />
-              ))}
+            <div className="list">
+                {displayMovies.map( (movie, index) => (
+                    <MovieCard 
+                      movie={movie}
+                      key={`movies-${index}`}
+                      dispatch={this.props.store.dispatch}
+                      isFavorite = {this.isMovieFavourite(movie)}
+                    />
+                ))}
+            </div>
+            {displayMovies.length === 0 ? <div className="no-movies">No movies to display! </div> : null}
           </div>
-          {displayMovies.length === 0 ? <div className="no-movies">No movies to display! </div> : null}
         </div>
-      </div>
-    );
-  }
+      );
+    }
 }
 
 export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,7 +6,9 @@ const initialMoviesState = {
     favourites: [],
     showFavourites: false
 }
-export default function movies(state = initialMoviesState, action){
+export function movies(state = initialMoviesState, action){
+
+    console.log('MOVIES REDUCER');
     // if(action.type === ADD_MOVIES){
     //     return {
     //         ...state,
@@ -44,3 +46,30 @@ export default function movies(state = initialMoviesState, action){
             return state;      
     }
 }
+
+
+
+
+const initialSearchState = {
+    result : {}
+};
+
+export function search(state = initialSearchState , action){
+    console.log('SEARCH REDUCER')
+    return state;
+}
+
+
+
+const initialRootState = {
+    movies: initialMoviesState,
+    search: initialSearchState
+}
+
+export default function rootReducer(state = initialRootState, action) {
+    return {
+        movies: movies(state.movies, action),
+        search: search(state.search, action)
+    }
+}
+
